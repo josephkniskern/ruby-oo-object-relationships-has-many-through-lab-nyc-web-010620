dc8ac1904ca0fd0851bee25ef3fcd4217b31cf58
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-13 19:52:14 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,5 @@
+
+
 class Patient
 
   attr_accessor :name
@@ -22,7 +24,7 @@ class Patient
   end
 
   def doctors
-    appointments.map { |appt| appt.doctor }
+    appointments.map { |appt| appt.date }
   end
 
 end
